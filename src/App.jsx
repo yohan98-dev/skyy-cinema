ecc4497b742febffa15939effb96844bb3e8c44e
Yohan98_akvasoft <avishkay@akvasoft.com>
2025-03-06 10:22:38 +0530

    api call and movie card created

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,87 @@
-import Search from "./components/Search";
-import { useState } from "react";
+import Search from './components/Search';
+import Spinner from './components/Spinner';
+import { use, useEffect, useState } from 'react';
 
+const API_BASE_URL = 'https://api.themoviedb.org/3';
+const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
+const API_OPTIONS = {
+  method: 'GET',
+  headers: {
+    accept: 'application/json',
+    Authorization: `Bearer ${API_KEY}`,
+  },
+};
 function App() {
-const [searchTerm, setSearchTerm] = useState("");
-  return ( 
+  const [errorMsg, setErrorMsg] = useState('');
+  const [movies, setMovies] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [searchTerm, setSearchTerm] = useState('');
+
+  const fetchMovies = async () => {
+    setIsLoading(true);
+    setErrorMsg('');
+    try {
+      const endpoint = `${API_BASE_URL}/discover/movie?include_adult=true&include_video=true&language=en-US&page=1&sort_by=popularity.desc`;
+      const response = await fetch(endpoint, API_OPTIONS);
+      if (response.ok) {
+        const data = await response.json();
+        if (data.Response == false) {
+          setErrorMsg(data.Error || 'Error fetching Movies. Try again later.');
+          setMovies([]);
+          return;
+        }
+        setMovies(data.results || []);
+      }
+    } catch (error) {
+      console.error(error);
+      setErrorMsg('Error fetching Movies. Try again later.');
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchMovies();
+  }, []);
+
+  return (
     <main>
       <div className="pattern" />
       <div className="wrapper">
         <header>
           <h1>
             <img src="/src/assets/hero.png" alt="hero" />
-            Find <span className="text-gradient">Movies</span> That you Love without the Hassle
+            Find <span className="text-gradient">Movies</span> That you Love
+            Without The Hassle
           </h1>
           <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
-          <h1 className="text-white">{searchTerm}</h1>
         </header>
+        <section className="all-movies">
+          <h2 className="mt-[40px]">All Movies</h2>
+          {isLoading ? (
+            <Spinner />
+          ) : errorMsg ? (
+            <p className="text-red-500">{errorMsg}</p>
+          ) : (
+            <ul>
+              {movies.map((movie) => (
+                <li key={movie.id} className="movie-card">
+                  <img
+                    src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+                    alt={movie.title}
+                  />
+                  <div>
+                    <h3>{movie.title}</h3>
+                    <p>{movie.overview}</p>
+                  </div>
+                </li>
+              ))}
+            </ul>
+          )}
+        </section>
       </div>
     </main>
-   
-   );
+  );
 }
 
 export default App;
--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,20 +1,21 @@
-import React from "react";
+import React from 'react';
 
-function Search({searchTerm, setSearchTerm}) {
-    return ( 
-    <div >
-        <div className="search">
-            <div>
-                <img src="/src/assets/search.svg" alt="search-logo" />
-                <input 
-                type="text" 
-                placeholder="Search Trough Thousends of Movies..." 
-                value={searchTerm} 
-                onChange={(e) => setSearchTerm(e.target.value)}
-                />
-            </div>
+function Search({ searchTerm, setSearchTerm }) {
+  return (
+    <div>
+      <div className="search">
+        <div>
+          <img src="/src/assets/search.svg" alt="search-logo" />
+          <input
+            type="text"
+            placeholder="Search Trough Thousends of Movies..."
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
+          />
         </div>
-    </div> );
+      </div>
+    </div>
+  );
 }
 
 export default Search;
